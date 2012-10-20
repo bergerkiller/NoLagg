78499e4c59c96b257346f058fbeb86fd7a582c4e
Irmo van den Berge <bergerkiller@gmail.com>
2012-10-21 01:38:00 +0200

    Defaulted spawn limiter to be disabled, new people forgetting to change limits

--- src/com/bergerkiller/bukkit/nolagg/NoLaggComponents.java
+++ src/com/bergerkiller/bukkit/nolagg/NoLaggComponents.java
@@ -12,7 +12,7 @@ public enum NoLaggComponents {
 	ITEMBUFFER("ItemBuffer", "Buffers items in chunks to prevent lag-outs because of lots of items"), 
 	ITEMSTACKER("ItemStacker", "Stacks nearby items to counter item-drop spammers and reduce item count on the server"), 
 	LIGHTING("Lighting", "Attempts to fix block and sky lighting bugs in worlds"), 
-	SPAWNLIMITER("SpawnLimiter", "Keeps entity counts below multiple configured thresholds"), 
+	SPAWNLIMITER("SpawnLimiter", "Keeps entity counts below multiple configured thresholds", false), 
 	TNT("TNT", "Replaces explosion creation with a faster version and buffers TNT ignites to prevent TNT server crashes"), 
 	SAVING("Saving", "Alters the way worlds are saved to reduce disk usage and to force proper saves"), 
 	COMMON("Common", "Common features such as the clear and garbage collect commands"), 
@@ -20,17 +20,23 @@ public enum NoLaggComponents {
 	THREADCHECK("ThreadCheck", "Notifies when a main-thread only event is called from another thread to detect instabilities it may cause");
 
 	private boolean enabled;
+	private final boolean enabledByDefault;
 	private String name, mainclass, description;
 
 	private NoLaggComponents(String name, String description) {
-		this(name, "com.bergerkiller.bukkit.nolagg." + name.toLowerCase() + ".NoLagg" + name, description);
+		this(name, description, true);
 	}
 
-	private NoLaggComponents(String name, String mainclass, String description) {
+	private NoLaggComponents(String name, String description, boolean enabledByDefault) {
+		this(name, "com.bergerkiller.bukkit.nolagg." + name.toLowerCase() + ".NoLagg" + name, description, enabledByDefault);
+	}
+
+	private NoLaggComponents(String name, String mainclass, String description, boolean enabledByDefault) {
 		this.enabled = false;
 		this.name = name;
 		this.mainclass = mainclass;
 		this.description = description;
+		this.enabledByDefault = enabledByDefault;
 	}
 
 	protected static void loadAll(FileConfiguration config) {
@@ -48,7 +54,7 @@ public enum NoLaggComponents {
 		ConfigurationNode node = config.getNode(this.name.toLowerCase());
 		node.setHeader("\n" + description);
 		node.setHeader("enabled", "Whether " + name + " should be loaded on startup");
-		if (node.get("enabled", true)) {
+		if (node.get("enabled", this.enabledByDefault)) {
 			try {
 				Class<?> clazz = Class.forName(this.mainclass);
 				NoLaggComponent comp = (NoLaggComponent) clazz.newInstance();
