53fda4eb34961a9d6d025dd21a7319f1cf93a5f1
Lennart <0p1q9o2w@hotmail.nl>
2013-01-20 13:41:20 +0100

    Pointed all entity functions to NativeUtil

--- src/main/java/com/bergerkiller/bukkit/nolagg/tnt/CustomExplosion.java
+++ src/main/java/com/bergerkiller/bukkit/nolagg/tnt/CustomExplosion.java
@@ -285,7 +285,7 @@ public class CustomExplosion {
 			double force = (1.0 - distanceFactor) * (double) damageFactor;
 
 			// CraftBukkit start - explosion damage hook
-			org.bukkit.entity.Entity damagee = entity.getBukkitEntity();
+			org.bukkit.entity.Entity damagee = NativeUtil.getEntity(entity);
 			int damageDone = (int) (force * (force + 1.0) * 4.0D * tmpsize + 1.0D);
 
 			if (this.source == null) { // Block explosion
@@ -302,7 +302,7 @@ public class CustomExplosion {
 					entity.motZ += tmpZ * force;
 				}
 			} else {
-				final org.bukkit.entity.Entity damager = this.source.getBukkitEntity();
+				final org.bukkit.entity.Entity damager = NativeUtil.getEntity(this.source);
 				final EntityDamageEvent.DamageCause damageCause;
 				if (damager instanceof TNTPrimed) {
 					damageCause = EntityDamageEvent.DamageCause.BLOCK_EXPLOSION;
@@ -334,7 +334,7 @@ public class CustomExplosion {
 	public void doBlocks() {
 		// CraftBukkit start
 		org.bukkit.World bworld = this.world.getWorld();
-		org.bukkit.entity.Entity explode = this.source == null ? null : this.source.getBukkitEntity();
+		org.bukkit.entity.Entity explode = this.source == null ? null : NativeUtil.getEntity(this.source);
 		Location location = new Location(bworld, this.posX, this.posY, this.posZ);
 
 		// generate org.bukkit Block array
